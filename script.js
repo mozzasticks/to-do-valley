8cc20a413cd852d22643101854368f526d3db156
mozzasticks <161789964+mozzasticks@users.noreply.github.com>
2024-10-15 00:20:42 -0400

    Create checkboxes and delete button for tasks

--- script.js
+++ script.js
@@ -15,9 +15,27 @@ todayTaskForm.addEventListener('submit', function (event) {
 
 function addTodayTask(task) {
     const listItem = document.createElement('li');
-    listItem.textContent = task;
-    //
+    const taskText = document.createElement('span');
+    taskText.textContent = task;
+    listItem.appendChild(taskText);
+
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    listItem.appendChild(checkbox);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    listItem.appendChild(deleteButton);
+
     todayTaskList.appendChild(listItem);
+    //
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            taskText.style.textDecoration = 'line-through';
+        } else {taskText.style.textDecoration = 'none';}
+    });
+
+    deleteButton.addEventListener('click', function() {todayTaskList.removeChild(listItem);});
 }
 
 //TODO TASKS
@@ -36,9 +54,27 @@ todoTaskForm.addEventListener('submit', function (event) {
 
 function addTodoTask(task) {
     const listItem = document.createElement('li');
-    listItem.textContent = task;
-    //
+    const taskText = document.createElement('span');
+    taskText.textContent = task;
+    listItem.appendChild(taskText);
+
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    listItem.appendChild(checkbox);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    listItem.appendChild(deleteButton);
+
     todoTaskList.appendChild(listItem);
+    //
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            taskText.style.textDecoration = 'line-through';
+        } else {taskText.style.textDecoration = 'none';}
+    });
+
+    deleteButton.addEventListener('click', function() {todoTaskList.removeChild(listItem);});
 }
 
 //HEALTH TASKS
@@ -57,7 +93,25 @@ healthTaskForm.addEventListener('submit', function (event) {
 
 function addHealthTask(task) {
     const listItem = document.createElement('li');
-    listItem.textContent = task;
-    //
+    const taskText = document.createElement('span');
+    taskText.textContent = task;
+    listItem.appendChild(taskText);
+
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    listItem.appendChild(checkbox);
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    listItem.appendChild(deleteButton);
+
     healthTaskList.appendChild(listItem);
+    //
+    checkbox.addEventListener('change', function() {
+        if (this.checked) {
+            taskText.style.textDecoration = 'line-through';
+        } else {taskText.style.textDecoration = 'none';}
+    });
+
+    deleteButton.addEventListener('click', function() {healthTaskList.removeChild(listItem);});
 }
